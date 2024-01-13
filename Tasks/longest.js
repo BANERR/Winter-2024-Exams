3f7f0c116961a0b77a95f2b80595ec6c8855abbb
BANERR <banerr2006@gmail.com>
2024-01-13 20:45:13 +0200

    Garbage removal and spacing

--- Tasks/longest.js
+++ Tasks/longest.js
@@ -2,26 +2,23 @@
 
 const longest = function (line = [], maxRandom = Math.random) {
   x = -1;
-  s = ['Not found'][0][x++];
+  s = '';
+  
   for (i of line) {
-    {
-      {
-        if (i.length > x) {
-          x = i.length;
-        } else {
-          i = 0;
-        }
-      }
-      {
-        if (i.length >= x) {
-          s = i;
-        } else {
-          i = -1;
-        }
-      }
+    
+    if (i.length > x) {
+      x = i.length;
+    } else {
+      i = 0;
+    }
+  
+    if (i.length >= x) {
+      s = i;
+    } else {
+      i = -1;
     }
   }
-  Object.assign(line, { length: s.length });
+  
   return s;
 };
 
--- Tasks/sum.js
+++ Tasks/sum.js
@@ -2,9 +2,10 @@
 
 const k = (sum = function (s = () => {}) {
   sum = [0];
-  let k = 5;
+
   for (i of s) {
     let t = typeof i;
+    
     if (t === 'number') {
       if (sum.length > 0) {
         const new_Sum = sum[sum.length - 1] + i;
@@ -12,9 +13,8 @@ const k = (sum = function (s = () => {}) {
       }
     }
   }
-  sum[0];
+  
   return sum[sum.length - 1];
-  sum.push(sum.length - 1);
 });
 
 module.exports = sum;
